3ae324bcf3348d0e5f8b28509e8527278bbb9c9e
Oganbelema <oganbelema@gmail.com>
2022-08-23 20:22:55 +0100

    testing out child and parent job cancellation

--- src/main/kotlin/Jobs.kt
+++ src/main/kotlin/Jobs.kt
@@ -5,8 +5,24 @@ import kotlinx.coroutines.runBlocking
 fun main() {
     runBlocking {
         val job1 = launch {
-            delay(3000L)
+           // delay(3000L)
             println("Job1 launched")
+
+            val job2 = launch {
+                println("Job2 launched")
+                delay(3000L)
+                println("Job2 is finishing")
+            }
+
+            job2.invokeOnCompletion { println("Job2 is completed") }
+
+            val job3 = launch {
+                println("Job3 launched")
+                delay(3000L)
+                println("Job3 is finishing")
+            }
+
+            job3.invokeOnCompletion { println("Job3 is completed") }
         }
 
         job1.invokeOnCompletion {
