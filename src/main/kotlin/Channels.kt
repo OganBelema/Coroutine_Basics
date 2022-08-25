f64205bf60f96a4b3e92daacf67c9c6bb5485dac
Oganbelema <oganbelema@gmail.com>
2022-08-25 14:50:26 +0100

    receiving channel inputs using channel's iterable and exploring close

--- src/main/kotlin/Channels.kt
+++ src/main/kotlin/Channels.kt
@@ -1,17 +1,37 @@
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
-        val channel = Channel<Int>()
+        //sendAndReceive()
 
-        launch {
-            for (x in 1..5)
-                channel.send(x * x)
-        }
-
-        for (i in 1..5)
-            println(channel.receive())
+        sendAndClose()
     }
+}
+
+private suspend fun CoroutineScope.sendAndReceive() {
+    val channel = Channel<Int>()
+
+    launch {
+        for (x in 1..5)
+            channel.send(x * x)
+    }
+
+    for (i in 1..5)
+        println(channel.receive())
+}
+
+private suspend fun CoroutineScope.sendAndClose() {
+    val channel = Channel<Int>()
+
+    launch {
+        for (x in 1..5)
+            channel.send(x * x)
+        channel.close()
+    }
+
+    for (i in channel)
+        println(i)
 }
