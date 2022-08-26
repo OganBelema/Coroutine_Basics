8d3e15bf2e2525bccf36a3dd9c7bb33fa465064b
Oganbelema <oganbelema@gmail.com>
2022-08-26 08:46:45 +0100

    explored buffered channels

---
+++ src/main/kotlin/Buffered Channels.kt
@@ -0,0 +1,27 @@
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * A buffered channel has a limited capacity
+ * When the capacity is reached, the sender is paused
+ * When capacity becomes available, new values can be sent
+ * */
+fun main() {
+    runBlocking {
+        val channel = Channel<Int>(4)
+        val sender = launch {
+            repeat(10) {
+                channel.send(it)
+                println("Sent $it")
+            }
+        }
+
+        repeat(3) {
+            delay(1000)
+            println("Received ${channel.receive()}")
+        }
+        sender.cancel()
+    }
+}
