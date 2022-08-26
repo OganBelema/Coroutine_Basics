b14e01a3b0cf68e870bf78fa697321d30adb1d2b
Oganbelema <oganbelema@gmail.com>
2022-08-26 09:19:44 +0100

    learnt about the shared state problem in coroutines

---
+++ src/main/kotlin/coroutine/Shared State Problem.kt
@@ -0,0 +1,41 @@
+package coroutine
+
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+/**
+ * Multiple coroutines can update a shared state variable concurrently
+ * However, some updates may be lost
+ * */
+fun main() {
+    runBlocking {
+        var counter = 0
+        withContext(Dispatchers.Default) {
+            massiveRun {
+                counter++
+            }
+        }
+        println("Counter = $counter")
+    }
+}
+
+//This shows the shared state problem
+//expected counter result is 100,000
+//actual result was 59100 in my case
+suspend fun massiveRun(action: suspend () -> Unit) {
+    val n = 100
+    val k = 1000
+    val time = measureTimeMillis {
+        coroutineScope {
+            repeat(n) {
+                launch {
+                    repeat(k) {
+                        action()
+                    }
+                }
+            }
+        }
+    }
+
+    println("Completed ${n * k} actions in $time ms")
+}
